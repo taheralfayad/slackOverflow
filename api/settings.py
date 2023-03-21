aa20e03232542357ea03c651e4b0ceb4d4e0bcb0
Evan Goldsmith <evangoldsmith1@gmail.com>
2023-03-21 12:43:22 -0400

    Add basic card model and initial rest api setup

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=%_w3un5fmjy)%euy86ocq79uo3-=#ny%d278rd4x17h#6rpgi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
+    'core'
 ]
 
 MIDDLEWARE = [
--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    text = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    project = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return 'Card for %s' % self.project
+
+    def __repr__(self):
+        return '<Card: %r>' % self.project
+
