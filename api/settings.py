53d54b820802ddd3f5c5389a845964250947c144
Evan Goldsmith <Evan.Goldsmith@ucf.edu>
2023-03-21 11:24:30 -0400

    Change django configuration to reflect folder renaming

--- api/settings.py
+++ api/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'slackOverflow.urls'
+ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'slackOverflow.wsgi.application'
+WSGI_APPLICATION = 'api.wsgi.application'
 
 
 # Database
--- core/apps.py
+++ core/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SlackOverflowConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'slack_overflow'
+    name = 'core'
