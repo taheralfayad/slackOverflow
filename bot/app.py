7970eb73c5215af63227440a830f54fbc0575390
Zander Preston <zanpres@gmail.com>
2023-03-21 12:02:02 -0400

    Add framework for slash commands

--- bot/app.py
+++ bot/app.py
@@ -4,7 +4,8 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from dotenv import load_dotenv
 
-load_dotenv("../.env")
+directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(directory, '.env'))
 
 # Initializes your app with your bot token and signing secret
 app = App(
@@ -41,6 +42,36 @@ def action_button_click(body, ack, say):
     # Acknowledge the action
     ack()
     say(f"<@{body['user']['id']}> clicked the button")
+
+# Command that adds an issue to the database
+# I don't think this should be the name personally
+# Format: "/summarize [project] [issue title] [description]"
+@app.command("/summarize")
+def summarize_command(ack, say, command):
+    # Acknowledge command request first:
+    ack()
+    # Then respond
+    say(f"{command['text']}")
+
+# Command that adds an answer to an issue on the database
+# Temporary name for now
+# Format: "/answer [project] [issue] [description]"
+@app.command("/answer")
+def answer_command(ack, say, command):
+    # Acknowledge command request first:
+    ack()
+    # Then respond
+    say(f"{command['text']}")
+
+# Command that lists all of the available projects/issues
+# Could be problematic to code, I'm unsure
+# Format: "/list [issues|projects] <project>"
+@app.command("/summarize")
+def list_command(ack, say, command):
+    # Acknowledge command request first:
+    ack()
+    # Then respond
+    say(f"{command['text']}")
 
 # Start your app
 if __name__ == "__main__":
