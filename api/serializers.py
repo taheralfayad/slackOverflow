c3f2918bb6ebbb29ecc6963b31e386d47778945b
Ahmad Altaher Alfayad <93749906+taheralfayad@users.noreply.github.com>
2023-03-22 13:04:04 -0400

    Delete api directory

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from core.models import Card
-
-class CardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Card
-        fields = ["created", "text", "author", "project"]
-
--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cards', views.CardList.as_view()),
-]
